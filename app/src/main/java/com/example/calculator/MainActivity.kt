abf3d764c746855363f150471f4f201acd64a055
Bharat-Ghusi <bharatghusi@appscrip.co>
2023-06-15 19:31:22 +0530

    Fixed result TV, Expression TV and NullPointerException

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculator
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -35,28 +36,28 @@ class MainActivity : ComponentActivity() {
     //Record onClick
     private fun recordOnClick() {
         binding.run {
-            btn0.setOnClickListener { updateExpressionTv(btn0) }
-            btn1.setOnClickListener { updateExpressionTv(btn1) }
-            btn2.setOnClickListener { updateExpressionTv(btn2) }
-            btn3.setOnClickListener { updateExpressionTv(btn3) }
-            btn4.setOnClickListener { updateExpressionTv(btn4) }
-            btn5.setOnClickListener { updateExpressionTv(btn5) }
-            btn6.setOnClickListener { updateExpressionTv(btn6) }
-            btn7.setOnClickListener { updateExpressionTv(btn7) }
-            btn8.setOnClickListener { updateExpressionTv(btn8) }
-            btn9.setOnClickListener { updateExpressionTv(btn9) }
+            btn0.setOnClickListener { onDigitClick(btn0) }
+            btn1.setOnClickListener { onDigitClick(btn1) }
+            btn2.setOnClickListener { onDigitClick(btn2) }
+            btn3.setOnClickListener { onDigitClick(btn3) }
+            btn4.setOnClickListener { onDigitClick(btn4) }
+            btn5.setOnClickListener { onDigitClick(btn5) }
+            btn6.setOnClickListener { onDigitClick(btn6) }
+            btn7.setOnClickListener { onDigitClick(btn7) }
+            btn8.setOnClickListener { onDigitClick(btn8) }
+            btn9.setOnClickListener { onDigitClick(btn9) }
 
-            btnPlus.setOnClickListener { updateExpressionTv(btnPlus) }
-            btnMinus.setOnClickListener { updateExpressionTv(btnMinus) }
-            btnDivide.setOnClickListener { updateExpressionTv(btnDivide) }
-            btnProduct.setOnClickListener { updateExpressionTv(btnProduct) }
-            btnModulo.setOnClickListener { updateExpressionTv(btnModulo) }
+            btnPlus.setOnClickListener { onOperatorClick(btnPlus) }
+            btnMinus.setOnClickListener { onOperatorClick(btnMinus) }
+            btnDivide.setOnClickListener { onOperatorClick(btnDivide) }
+            btnProduct.setOnClickListener { onOperatorClick(btnProduct) }
+            btnModulo.setOnClickListener { onOperatorClick(btnModulo) }
 
-            btnAc.setOnClickListener { updateExpressionTv(btnAc) }
-            btnBackspace.setOnClickListener { updateExpressionTv(btnBackspace) }
-            btnEqualTO.setOnClickListener { updateExpressionTv(btnEqualTO) }
-            btnDot.setOnClickListener { updateExpressionTv(btnDot) }
-            btnExpand.setOnClickListener { updateExpressionTv(btnExpand) }
+            btnAc.setOnClickListener { onClearScreen(btnAc) }
+            btnEqualTO.setOnClickListener { onEqualToClick(btnEqualTO) }
+            btnBackspace.setOnClickListener { onBackSpaceClick(btnBackspace) }
+            btnDot.setOnClickListener { onDigitClick(btnDot) }
+            btnExpand.setOnClickListener { onExpandClick(btnExpand) }
         }
     }
 
@@ -66,59 +67,61 @@ class MainActivity : ComponentActivity() {
     }
 
 
-    //Update Expression Tv onClick
-    private fun updateExpressionTv(btn: Button) {
-        val btnText = btn.text.toString()
-        val expression = binding.textExpression.text.toString()
-        //case-1
-        if (btnText == "AC") {
-            clearScreen(binding.textExpression)
+
+
+    fun onDigitClick(view: View) {
+        //        set textExpression tv visibility
+        binding.textExpression.visibility = View.VISIBLE
+        //Set on exp Tv
+        binding.textExpression.text = binding.textExpression.text.toString() + (view as Button).text
+
+    }
+
+    fun onOperatorClick(view: View) {
+//        set textExpression tv visibility
+        binding.textExpression.visibility = View.VISIBLE
+        //Set on exp Tv
+        if(binding.textExpression.text.isEmpty()){
+            binding.textExpression.text = "0"
         }
-        //case-2
-        else if (btnText == "C" && binding.textExpression.text.toString().isNotEmpty()) {
-            binding.textExpression.text = binding.textExpression.text.substring(0,
-                binding.textExpression.text.toString().length - 1 )
-        }
+        binding.textExpression.text = binding.textExpression.text.toString() + (view as Button).text
+
+    }
+
+
+    fun onEqualToClick(view: View) {
         //case-3
-        else if (btnText == "=") {
-            clearScreen(binding.textExpression)
-            binding.textResult.text = eval.evaluate(expression)
-            clearScreen(binding.textExpression)
+
+        binding.textResult.text ="= " + eval.evaluate(binding.textExpression.text.toString())
+        binding.textExpression.text = binding.textResult.text.removePrefix("= ")
+        //Make expression TV invisible
+        binding.textExpression.visibility = View.INVISIBLE
+
+    }
+
+    fun onClearScreen(view: View) {
+        //        set textExpression tv visibility
+        binding.textExpression.visibility = View.VISIBLE
+        //case-1
+        binding.textExpression.text = ""
+        binding.textResult.text = "0"
+    }
+
+    fun onBackSpaceClick(view: View) {
+        //case-2
+        if (binding.textExpression.text.toString().isNotEmpty()) {
+            binding.textExpression.text = binding.textExpression.text.substring(
+                0,
+                binding.textExpression.text.toString().length - 1
+            )
         }
+    }
+
+    fun onExpandClick(view: View) {
         //case-4
-        else if (btnText == "EXP") {
-
-        }
-        //case-5 onClick digit and Operator
-        else if(btn.text.toString() != "C") {
-            //Set on exp Tv
-            binding.textExpression.text = binding.textExpression.text.toString() + btnText
-            //Set result on Result TV
-            doCalculate(prevResult, btnText, operator)
-        }
-
 
     }
 
-    private fun doCalculate(result: Double, newOperand: String, operator: String): Double {
-        setResultTv(result)
-        return result
-    }
-
-    //Check if token of expression is operator
-    private fun isOperator(ch: Char): Boolean {
-        return (ch == '+' || ch == '-' || ch == 'x' || ch == '/' || ch == '%')
-    }
-
-    //Set result Tv
-    private fun setResultTv(result: Double) {
-
-    }
-
-//    fun onOperatorClick(view: View) {
-//        operator = (view as Button).text.toString() //Update latest operator
-//
-//    }
 
 }
 
